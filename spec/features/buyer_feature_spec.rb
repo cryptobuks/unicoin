fdc9fb2b27645eb94a20737f0b9d577ecb1a2b2e
Jack <92jackhenderson@gmail.com>
2017-02-28 10:26:13 +0000

    updated routes and sign up tests passing

--- spec/features/buyer_feature_spec.rb
+++ spec/features/buyer_feature_spec.rb
@@ -5,13 +5,12 @@ describe 'Buyer' do
   context 'A buyer can sign up' do
     scenario 'a buyer fills in a form to sign up' do
       visit '/buyers/sign_up'
-      save_and_open_page
       fill_in 'Name', with: 'Mike'
       fill_in 'Email', with: 'mike@test.com'
       fill_in 'Password', with: 'abc123'
       fill_in 'Password confirmation', with: 'abc123'
       click_button 'Sign up'
-      expect(path).to eq '/'
+      expect(current_path).to eq '/'
     end
   end
 end
--- spec/features/seller_feature_spec.rb
+++ spec/features/seller_feature_spec.rb
@@ -5,13 +5,12 @@ describe 'Seller' do
   context 'A seller can sign up' do
     scenario 'a seller fills in a form to sign up' do
       visit '/sellers/sign_up'
-      save_and_open_page
       fill_in 'Name', with: 'Mike'
       fill_in 'Email', with: 'mike@test.com'
       fill_in 'Password', with: 'abc123'
       fill_in 'Password confirmation', with: 'abc123'
       click_button 'Sign up'
-      expect(path).to eq '/'
+      expect(current_path).to eq '/'
     end
   end
 end
