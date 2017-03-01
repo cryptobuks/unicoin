4c78e591aa15beb8b47e5edc95a11aef9b693b88
Sam Mckay <sammckay12@gmail.com>
2017-03-01 12:43:20 +0000

    1 offer for buyer only

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,22 +1,27 @@
 class OffersController < ApplicationController
   def index
     @offers = Offer.all
+    @buyer_offer = @offers.order(:price_per_coin).where(:status => 'Open').first
   end
 
   def new
-
   end
 
   def create
-    Offer.create(offers_params)
+    @offer = Offer.create(offers_params)
+    @offer.update(status: 'Open')
     redirect_to '/offers'
   end
 
   def update
     @offer = Offer.find(params[:id])
     @offer.update(status: 'Sold')
+    @buyer_offer = Offer.find(params[:id])
+    @buyer_offer.update(status: 'Sold')
     redirect_to offers_path
   end
+
+
 
 private
   def offers_params
